69196695e1399827e9306707391cbbb4dffa6d44
OskarGroth <oskar@cindori.org>
2016-08-31 12:57:12 +0200

    Initial Commit

---
+++ ColorPicker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ColorPicker
+//
+//  Created by Oskar Groth on 2016-08-31.
+//  Copyright © 2016 Cindori. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
